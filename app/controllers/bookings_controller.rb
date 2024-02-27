d5abe3ee189c777d590ae8066c6ed8e56d805ec9
Zena El Kurdi <zena.elkurdi@gmail.com>
2024-02-27 16:04:33 +0100

    on revient

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,8 +7,10 @@ class BookingsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     @booking = Booking.new(booking_params)
+    @booking.user = current_user
     @booking.book = @book
     if @booking.save
+      @book.update(status: "booked")
       redirect_to book_path(@book)
     else
       render :new, status: :unprocessable_entity
@@ -17,11 +19,12 @@ class BookingsController < ApplicationController
 
   def destroy
     @booking = Booking.find(params[:id])
+    @book = @booking.book
     @booking.destroy
-    redirect_to book_path(@booking.book), status: :see_other
+    redirect_to book_path(@book), status: :see_other
   end
 
   def booking_params
-    params.require(:booking).permit(:user_id, :book_id, :start_date, :end_date)
+    params.require(:booking).permit(:book_id, :start_date, :end_date)
   end
 end
