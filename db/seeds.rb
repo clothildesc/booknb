1c4d081dbe7bc53f04c09f7999872c942379c354
clothilde scache <clothilde.sca@gmail.com>
2024-02-28 15:01:59 +0100

    modified seeds with cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-import "faker"
+require "faker"
+require "open-uri"
 
 Book.destroy_all
 Booking.destroy_all
@@ -11,15 +12,17 @@ User.destroy_all
     )
 
     2.times do
-      Book.create!(
+      book = Book.create!(
         title: Faker::Book.title,
         author: Faker::Book.author,
         summary: Faker::Lorem.paragraph(sentence_count: 5),
         year: Faker::Number.between(from: 1900, to: Time.now.year),
         isbn_number: Faker::Number.leading_zero_number(digits: 13),
         editor: Faker::Book.publisher,
-        book_picture: Faker::LoremFlickr.image(size: "200x200", search_terms: ['books']),
         user: user
       )
+
+      file = URI.open(Faker::LoremFlickr.image(size: "200x200", search_terms: ['books']))
+      book.photo.attach(io: file, filename: "book_cover.png", content_type: "image/jpg")
     end
   end
