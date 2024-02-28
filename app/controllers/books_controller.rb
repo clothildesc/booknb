a9e8e14bb8428a640064905bf11bcfb5c7120994
Pierre Urban <pierreurban1@gmail.com>
2024-02-28 17:20:35 +0100

    changed the login's place

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @books = Book.all
