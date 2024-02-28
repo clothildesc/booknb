13ceb528c1fca7c1be4f1fa3bd29d0f3ac6e448f
Zena El Kurdi <zena.elkurdi@gmail.com>
2024-02-28 14:42:38 +0100

    map added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,13 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @users = User.all
+    @markers = @users.geocoded.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude
+      }
+    end
   end
 
   def show
