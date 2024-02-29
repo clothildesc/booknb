e4ad5221a8262427cd4ca5f29ea75d616e0916bd
Pierre Urban <pierreurban1@gmail.com>
2024-02-29 11:57:14 +0100

    updated the booking status to be coherent

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,14 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :book
+  after_create :booked_book_status
+  before_destroy :available_book_status
+
+  def booked_book_status
+    book.update!(status: "booked")
+  end
+
+  def available_book_status
+    book.update!(status: "available")
+  end
 end
