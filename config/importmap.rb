f2127dee54fc93e7d7041a3cb62f0e640a47bf0b
clothilde scache <clothilde.sca@gmail.com>
2024-03-01 12:59:01 +0100

    modified booking form with 2 inputs instead of one

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,5 @@ pin "@popperjs/core", to: "popper.js", preload: true
 pin "mapbox-gl" # @3.1.2
 pin "process" # @2.0.1
 pin "flatpickr" # @4.6.13
+pin "flatpickrRangePlugin", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/di
+st/esm/plugins/rangePlugin.js"
